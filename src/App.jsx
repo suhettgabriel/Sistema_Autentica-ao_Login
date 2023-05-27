edc339ccf30e123fdb361794e90d1471fd296b5e
suhettgabriel <suhettgabriel1983@gmail.com>
2023-05-27 08:42:58 -0300

    Criando projeto e iniciando formulário

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import LoginsPage from './pages/LoginPage';
+import HomePage from './pages/HomePAge';
+
+function App() {
+  return (
+    <div className="app">
+      <LoginsPage />
+    </div>
+  );
+}
+
+export default App;
