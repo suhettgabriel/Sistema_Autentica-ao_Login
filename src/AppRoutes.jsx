85ba1ab3dbba4a5df135fb429ebc8462b7bf7ab3
suhettgabriel <suhettgabriel1983@gmail.com>
2023-05-28 08:55:19 -0300

    Criação do contexto e implementação dele nas rotas

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,22 +1,28 @@
+import React, { useState } from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Navigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
 } from "react-router-dom";
 
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePAge';
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePAge";
+
+import { AuthProvider } from "./context/auth";
 
 const AppRoutes = () => {
-    return(
-        <Router>
-            <Routes>
-                <Route exact path="/login" element={<LoginPage/>}/>
-                <Route exact path="/" element={<HomePage/>}/>
-            </Routes>
-        </Router>
-    )
-}
+ 
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,46 +1,53 @@
-import React, {useState} from "react";
-import './styles.css';
+import React, { useState, useContext } from "react";
 
+import { AuthContext } from "../../context/auth";
+
+import "./styles.css";
 
 const LoginPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const { authenticated, login } = useContext(AuthContext);
 
-    const handleSumit = (e) => {
-        e.preventDefault();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        console.log("submit", {email, password});
-    };
-    return (
+  const handleSumit = (e) => {
+    e.preventDefault();
+
+    console.log("submit", { email, password });
+    login(email, password); // integração com meu contexto / api
+  };
+  
+  return (
     <div id="login">
-    <h1 className="title">Login do Sistema</h1>
-    <form className="form" onSubmit={handleSumit}>
-     <div className="field">
-        <label htmlFor="email">E-mail</label>
-        <input
+      <h1 className="title">Login do Sistema</h1>
+      <p>{String(authenticated)}</p>
+      <form className="form" onSubmit={handleSumit}>
+        <div className="field">
+          <label htmlFor="email">E-mail</label>
+          <input
             type="email"
             name="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="field">
-        <label htmlFor="password">Senha</label>
-        <input
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input
             type="password"
             name="password"
             id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}   
-         />
-      </div>    
-      <div className="actions">
-        <button type="submit">Entrar</button>  
-      </div>
-    </form>
-  </div>
-    )
-}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="actions">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
