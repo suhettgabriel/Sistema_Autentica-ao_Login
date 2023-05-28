a68f4125ae64cad8820cb5832610ee3136872163
suhettgabriel <suhettgabriel1983@gmail.com>
2023-05-28 09:26:12 -0300

    protegendo as rotas e usando o navigate

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,16 +9,31 @@ import {
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePAge";
 
-import { AuthProvider } from "./context/auth";
+import { AuthProvider, AuthContext } from "./context/auth";
 
 const AppRoutes = () => {
- 
+  const Private = ({ children }) => {
+    const { authenticated } = useContext(AuthContext);
+
+    if (!authenticated) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
   return (
     <Router>
       <AuthProvider>
         <Routes>
           <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/" element={<HomePage />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Private>
+                <HomePage />
+              </Private>
+            }
+          />
         </Routes>
       </AuthProvider>
     </Router>
--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -1,26 +1,32 @@
 import React, { useState, createContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    
-    const [user, setUser] = useState(null);
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
-    const login = (email, passaword) => {
-      console.log("login auth", { email, passaword });
+  const login = (email, passaword) => {
+    console.log("login auth", { email, passaword });
+    if (passaword === "secret") {
       setUser({ id: "123", email });
-    };
-  
-    const logout = () => {
-      console.log("logout");
-    };
+      navigate("/");
+    }
+  };
 
-    return(
-        <AuthContext.Provider
-        value={{ authenticated: !!user, user, login, logout }}
-      >
-        { children }
-      </AuthContext.Provider>
-    )
-}
+  const logout = () => {
+    console.log("logout");
+    setUser(null);
+    navigate("/login");
+  };
 
+  return (
+    <AuthContext.Provider
+      value={{ authenticated: !!user, user, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
